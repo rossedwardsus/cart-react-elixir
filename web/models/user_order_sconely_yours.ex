32616c0cf5caf6d8dce58329befec090e8a259d4
Ross Edwards <rossedwards.us@gmail.com>
2016-11-09 20:24:30 -0800

    sconely yours models

---
+++ web/models/user_order_sconely_yours.ex
@@ -0,0 +1,18 @@
+defmodule SconeHomeElixir.UserOrderItems do
+		#use Ecto.Schema
+		#import Ecto.Changeset
+
+		use SconeHomeElixir.Web, :model
+
+		@derive {Poison.Encoder, only: [:user_id, :email, :password]}
+
+		schema "order_items" do
+			#field :user_id, Ecto.UUID
+			field :order_id, :string
+			field :item_id, :string
+			
+			#timestamps()
+		end
+
+
+end 
