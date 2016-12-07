c8d5ef915677b8e2e52cbc5c8dea8af2d75955aa
Ross Edwards <rossedwards.us@gmail.com>
2016-12-07 11:01:05 -0800

    router

---
+++ web/static/js/typescript/components/sconely_yours/Hello.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import DatePicker from "react-datepicker";
+import moment = require('moment');
+import Dropzone = require('react-dropzone');
+import { Router, browserHistory } from 'react-router';
+ 
+
+export interface HelloProps { compiler: string; framework: string; }
+
+export class Hello extends React.Component<HelloProps, {}> {
+    render() {
+        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+    }
+}
