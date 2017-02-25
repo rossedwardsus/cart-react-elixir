642712e5b1b478dc49dd69b46e987ea0c563a925
Ross Edwards <rossedwards.us@gmail.com>
2017-02-24 16:31:08 -0800

    link

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -28176,7 +28176,7 @@ webpackJsonp([0],[
 	                ),
 	                _react2.default.createElement(
 	                  'button',
-	                  { type: 'button', onClick: this.addToCart(this.state.item), className: 'btn btn-default', 'data-dismiss': 'modal' },
+	                  { type: 'button', className: 'btn btn-default', 'data-dismiss': 'modal' },
 	                  'Add'
 	                )
 	              )
