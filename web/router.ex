1837b5a61badfc63bc3d9e6e37245e656df91738
Ross Edwards <rossedwards.us@gmail.com>
2017-04-18 10:26:16 -0700

    removed more routes

--- web/router.ex
+++ web/router.ex
@@ -54,62 +54,6 @@ defmodule SconeHomeElixir.Router do
     pipe_through :browser # Use the default browser stack
 
 
-
-    #social
-    get "/mobile/guest", MobileSconelySignatureGuestChooseItemController, :index
-    
-    #sconely yours
-    #get "/mobile/order/datetime", MobileOrderController, :index
-    
-    get "/mobile/user", MobileUserController, :index
-    #get "/mobile/user/sconely_yours/items", MobileUserSconelyYoursItemsController, :index
-    #get "/mobile/user/sconely_yours/payment", MobileUserSconelyYoursPaymentController, :index
-    #get "/mobile/user/sconely_yours/address", MobileUserSconelyYoursAddressController, :index
-    #get "/mobile/user/sconely_yours/datetime", MobileUserSconelyYoursDateTimeController, :index
-    #get "/mobile/user/sconely_yours", MobileUserSconelyYoursController, :index
-    
-    get "/mobile/menu", MobileMenuController, :index
-    get "/mobile/order", MobileOrderController, :index
-    get "/mobile/login", MobileLoginController, :index    
-    post "/mobile/login", MobileLoginController, :create   
-    post "/mobile/registration", MobileRegistrationController, :create
-    get "/mobile/registration/2fa", MobileRegistration2faController, :index
-    post "/mobile/registration/2fa", MobileRegistration2faController, :create
-   
-
-    get "/mobile", MobileController, :index
-
-
-    
-    get "/", HomeController, :index
-    get "/login", LoginController, :index
-    post "/login", LoginController, :create
-    get "/register", RegisterController, :index
-    post "/register", RegisterController, :create
-    
-
-    #not used anymore
-    #get "/redux", ReduxController, :index
-
-
-
-    #not used anymore
-    #get "/user", UserController, :index
-    #get "/user/settings", UserSettingsController, :index
-    #get "/user/order", UserOrderController, :index
-    #get "/user/order/wizard", UserOrderWizardController, :index
-    
-    #get "/user/profile/photo", UserProfilePhotoController, :index
-    #get "/user/profile/delivery_address", UserProfileDeliveryAddressController, :index
-    #get "/user/profile/name", UserProfileNameController, :index
-    #get "/user/profile/food_preference", UserProfileFoodPreferenceController, :index
-    #get "/user/profile/payment", UserProfilePaymentController, :index
-
-    #get "/user/profile/payment", UserProfilePaymentController, :index
-
-    #get "/order/:order_id/guests/item", UserOrderGuestChooseItemController, :index
-
-    #resources "/posts", PostController
     
   end
 
