306a44372dafb3c3b36f8d869d6680b7815e9d33
Ross Edwards <rossedwards@Rosss-MacBook-Pro.local>
2017-07-21 16:29:59 -0700

    guest menu items

--- web/static/js/flow/signature_guest_response/menu_items.tsx
+++ web/static/js/flow/signature_guest_response/menu_items.tsx
@@ -194,10 +194,11 @@ class GuestMenu extends React.Component<any, any> {
                       <br/>
                       {this.props.sconely_message}
                       <br/>
+                      <br/>
+                      <br/>
+                      <br/>
+                      <br/>
                       <img src="/images/menu/LACI_1.5.jpg"/>
-                      <br/>
-                      <br/>
-                      <br/>
                       <br/>
                       <br/>
                       <br/>
