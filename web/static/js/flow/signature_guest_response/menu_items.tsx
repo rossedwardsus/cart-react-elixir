561201bd7b215c2b0e7c2a32088fff6e1b6c85b6
Ross Edwards <rossedwards@Rosss-MacBook-Pro.local>
2017-07-25 15:27:40 -0700

    email

--- web/static/js/flow/signature_guest_response/menu_items.tsx
+++ web/static/js/flow/signature_guest_response/menu_items.tsx
@@ -314,8 +314,6 @@ class GuestMenu extends React.Component<any, any> {
                         </div>
                         <br/>
                         <div className="col-xs-12 hidden-lg" style={{fontSize: 12, wordWrap: "break-word", overflowWrap: "break-word", textOverflow: "ellipsis"}}>
-                            <button className="btn btn-default" onClick={() => this.noThanks()}>No Scone for me, thanks!</button>
-                            <br/>
                             <br/>
                             All of our scones are made from scratch and freshly baked to order.  Our scones are free of refined sugars, soy and chemical preservatives. Although we offer gluten-free, nut-free and dairy free options, we cannot guarantee that any of our scones do not contain traces of allergens (including dairy, eggs, soy, tree nuts, wheat and others),  as we bake in a common kitchen where shared equipment is used.
                             <br/>
