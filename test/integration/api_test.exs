4c5905b2ee4854938113243675a6a3b1b3b76a07
Ross Edwards <rossedwards@Rosss-MacBook-Pro.local>
2017-08-21 21:12:23 -0700

    unit tests

---
+++ test/integration/api_test.exs
@@ -0,0 +1,31 @@
+defmodule Sconely.ApiTest do
+  use SconeHomeElixir.ConnCase, async: true
+  #use Plug.Test
+  alias SconeHomeElixir.Router
+
+  @opts Router.init([])
+  test 'posting to login api with nothing' do
+    conn = build_conn(:post, "/api/login")
+    #response = Router.call(conn, @opts)
+    #IO.inspect(response)
+    #assert response.status == 200
+  end
+
+  test 'graphql - public menu' do
+    #axios.post('/api/graphql',
+    # {query: 'query {get_menu_items {id name description ingredients}}'}
+    
+    #IO.puts("\n")
+     
+    conn = build_conn(:post, "/api/graphql", %{"query" => "query {get_menu_items {id name description ingredients}}"})
+
+    IO.puts("\n")
+
+    #conn = build_conn(:get, "/api/login")
+    response = Router.call(conn, @opts)
+    #IO.inspect(response)
+    
+    assert response.status == 200
+  end
+
+end
