0c817b27f7d3c97e38cc7878ccb9f0035e3bfe93
Ross Edwards <rossedwards@Rosss-MacBook-Pro.local>
2017-09-18 16:50:12 -0700

    menu view button

--- web/static/js/flow/order_menu.tsx
+++ web/static/js/flow/order_menu.tsx
@@ -537,7 +537,7 @@ class OrderMenu extends React.Component<any, any> {
                                 return(
                                         <div className="col-xs-12 col-md-4 col-lg-4" style={{marginTop: 0, marginBottom: 0}}>
                                           <div>
-                                              <img id="1" onClick={() => this.showItem(item.id)} onMouseEnter={(e: any) => e.currentTarget.src="/images/menu/" + item.name.toLowerCase().replace(/ /g, "") + "rollover.jpg"} onMouseLeave={(e: any) => e.currentTarget.src="/images/menu/" + item.name.toLowerCase().replace(/ /g, "") + ".jpg"} src={"/images/menu/" + item.name.toLowerCase().replace(/ /g, "") + ".jpg"} data-target="myModal" alt="..." height="270" width="270"/>
+                                              <img id="1" onClick={() => this.showItem(item.id)} onMouseEnter={(e: any) => e.currentTarget.src="/images/menu/" + item.name.toLowerCase().replace(/ /g, "") + "rollover.jpg"} onMouseLeave={(e: any) => e.currentTarget.src="/images/menu/" + item.name.toLowerCase().replace(/ /g, "") + ".jpg"} src={"/images/menu/" + item.name.toLowerCase().replace(/ /g, "") + ".jpg"} data-target="myModal" alt="..." height="350" width="350"/>
                                           </div>
                                           <div>
                                             <div className="col-xs-12 col-md-7 col-lg-6" style={{float: "left"}}> 
