efbedc8d65d1b4d19dde08afcf4de73e9d24e9ec
Ross Edwards <rossedwards@Rosss-MacBook-Pro.local>
2017-09-18 16:30:09 -0700

    menu view button

--- web/static/js/flow/order_menu.tsx
+++ web/static/js/flow/order_menu.tsx
@@ -536,10 +536,16 @@ class OrderMenu extends React.Component<any, any> {
 
                                 return(
                                         <div className="col-xs-12 col-md-4 col-lg-4" style={{marginTop: 0, marginBottom: 0}}>
+                                          <div>
                                               <img id="1" onClick={() => this.showItem(item.id)} onMouseEnter={(e: any) => e.currentTarget.src="/images/menu/" + item.name.toLowerCase().replace(/ /g, "") + "rollover.jpg"} onMouseLeave={(e: any) => e.currentTarget.src="/images/menu/" + item.name.toLowerCase().replace(/ /g, "") + ".jpg"} src={"/images/menu/" + item.name.toLowerCase().replace(/ /g, "") + ".jpg"} data-target="myModal" alt="..." height="270" width="270"/>
+                                          </div>
                                           <br/>
                                           <br/>
-                                          <b>{item.name}</b>
+                                          <div>
+                                            <div className="col-xs-12 col-md-10 col-lg-10 pull-left"> 
+                                              <b>{item.name}</b>
+                                            </div>
+                                          </div>
                                           <br/>
                                           <br/>
                                           <br/>
