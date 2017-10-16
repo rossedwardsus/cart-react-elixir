dde02564e749b67138425d007665a9f73b5e0922
Ross Edwards <rossedwards@Rosss-MacBook-Pro.local>
2017-10-15 20:16:30 -0700

    mobile select

--- web/static/js/flow/order_menu.tsx
+++ web/static/js/flow/order_menu.tsx
@@ -561,24 +561,17 @@ class OrderMenu extends React.Component<any, any> {
 
       yours_social_pool_quantity_selector =  <div>
                                                 <div className="col-xs-12 col-md-12">
-                                                    <div className="col-xs-4 col-sm-4 col-md-4" style={{paddingLeft: "0px"}}>
                                                       <select className="form-control" value={this.state.selected_item_size} onChange={this.selectedItemSize} style={{borderRadius: 0, height: 35, width: 150}}>
                                                         <option value="">Mini or Regular</option> 
                                                         <option value="regular">1 Dozen Regular</option>
                                                         <option value="mini">2 Dozen Minis</option>
                                                       </select>
-                                                    </div>
-                                                    <div className="col-xs-4 col-md-3">
                                                       <select className="form-control" value={this.state.selected_item_quantity_selector} onChange={this.selectedItemQuantity} style={{borderRadius: 0, height: 35, width: 100}}>
                                                         <option>Quantity</option> 
                                                         {selected_item_quantity_options_array.map((value: any) => <option value={value}>{value}</option>)}
                                                       </select>
-                                                    </div>
-                                                    <div className="col-md-4">
                                                       <button className={this.state.add_cart_item_button_classname}  type="button" onClick={() => this.addCartItem()} style={{borderRadius: 0, WebkitAppearance: "none", height: 35, width: 50}}>Add</button>
-                                                    </div>
-                                                    
-                                                    
+                                              
                                                 </div>
                                             </div>
 
@@ -686,11 +679,6 @@ class OrderMenu extends React.Component<any, any> {
                         </div>
                         <div className="modal-footer">
                           <form className="form-horizontal">
-                                <select className="form-control" value={this.state.selected_item_size} onChange={this.selectedItemSize} style={{borderRadius: 0, height: 35, width: 150, cursor: "pointer"}}>
-                                <option value="">Mini or Regular</option> 
-                                <option value="regular">1 Dozen Regular</option>
-                                <option value="mini">2 Dozen Minis</option>
-                              </select>
                                 {yours_social_pool_quantity_selector}
                           </form>
                         </div>
