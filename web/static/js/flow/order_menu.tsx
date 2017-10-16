1c6b75aa79b68b7b800f294c3dc0099655cffdeb
Ross Edwards <rossedwards@Rosss-MacBook-Pro.local>
2017-10-15 21:01:06 -0700

    mobile select

--- web/static/js/flow/order_menu.tsx
+++ web/static/js/flow/order_menu.tsx
@@ -570,7 +570,7 @@ class OrderMenu extends React.Component<any, any> {
             //}else{
 
       yours_social_pool_quantity_selector =  <div>
-                                                <div className="col-xs-12 col-md-12">
+                                                <div className="col-md-12">
                                                       <div className="col-md-4">
                                                       <select className="form-control" value={this.state.selected_item_size} onChange={this.selectedItemSize} style={{borderRadius: 0, height: 35, width: 150}}>
                                                         <option value="">Mini or Regular</option> 
@@ -580,14 +580,16 @@ class OrderMenu extends React.Component<any, any> {
                                                       </div>
                                                       <div className="hidden-lg col-xs-1">
                                                         <br/>
+                                                        <br/>
                                                       </div>
                                                       <div className="col-md-4">
-                                                      <select className="form-control" value={this.state.selected_item_quantity_selector} onChange={this.selectedItemQuantity} style={{borderRadius: 0, height: 35, width: 100}}>
+                                                      <select className="form-control" value={this.state.selected_item_quantity_selector} onChange={this.selectedItemQuantity} style={{borderRadius: 0, height: 35, width: 100, WebkitAppearance: "none"}}>
                                                         <option>Quantity</option> 
                                                         {selected_item_quantity_options_array.map((value: any) => <option value={value}>{value}</option>)}
                                                       </select>
                                                       </div>
                                                       <div className="hidden-lg col-xs-1">
+                                                        <br/>
                                                         <br/>
                                                       </div>
                                                       <div className="col-md-4">
