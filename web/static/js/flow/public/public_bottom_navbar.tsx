cfb75b7677f36d3ce2c41028d08d6a8523121fa7
Ross Edwards <rossedwards@Rosss-MacBook-Pro.local>
2017-10-18 11:08:39 -0700

    text

--- web/static/js/flow/public/public_bottom_navbar.tsx
+++ web/static/js/flow/public/public_bottom_navbar.tsx
@@ -221,7 +221,7 @@ export default class Menu extends React.Component<any, any> {
                   <div className="hidden-xs col-md-3">
                       <div className="form-horizontal" style={{paddingLeft: 0}}>
                         <div className="col-md-5" style={{paddingLeft: 10}}>
-                          <div style={{fontSize: 15, height: 10}}>eat@sconely.com</div>
+                          <div style={{fontSize: 15, height: 10}}>We'd love to hear from you!<br/>eat@sconely.com</div>
                         </div>
                       </div>
                       <br/>
