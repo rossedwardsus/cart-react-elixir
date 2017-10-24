4bdb46c4249a792bb67052b8d41a1ffcfdf197c1
Ross Edwards <rossedwards@Rosss-MacBook-Pro.local>
2017-10-24 08:43:41 -0700

    config

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :scone_home_elixir, SconeHomeElixir.Endpoint,
   url: [scheme: "https", host: "sconely.herokuapp.com", port: 443],
 		force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   http: [compress: true],
   https: [compress: true]
 
