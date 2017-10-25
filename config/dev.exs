608531c5b942ae8c34ababde84ea4f85f6bc1c8e
Ross Edwards <rossedwards@Rosss-MacBook-Pro.local>
2017-10-25 12:22:38 -0700

    image and promo code

--- config/dev.exs
+++ config/dev.exs
@@ -9,7 +9,7 @@ use Mix.Config
 config :scone_home_elixir, SconeHomeElixir.Endpoint,
   http: [port: 4000],
   debug_errors: true,
-  #code_reloader: true,
+  code_reloader: true,
   cache_static_lookup: false,
   check_origin: false
   #watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin"]]
