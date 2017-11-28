d6f571fe301558171eaa04d7cf4310d822ff0040
Ross Edwards <rossedwards@Rosss-MacBook-Pro.local>
2017-11-28 07:25:47 -0800

    browser logging

--- web/static/js/flow/public/public_home_page.tsx
+++ web/static/js/flow/public/public_home_page.tsx
@@ -75,8 +75,8 @@ export class PublicHomePage extends React.Component<any, any> {
 
     }else{
 
-        this.setState({fontSizeLarge: 18});
-        this.setState({fontSizeSmall: 17});        
+        this.setState({fontSizeLarge: 16});
+        this.setState({fontSizeSmall: 15});        
 
     }
 
