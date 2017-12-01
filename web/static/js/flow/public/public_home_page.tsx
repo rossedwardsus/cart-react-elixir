e3e7b0e0773aa5efd2f75d752cc95a2032127403
Ross Edwards <rossedwards@Rosss-MacBook-Pro.local>
2017-11-30 20:47:08 -0800

    press

--- web/static/js/flow/public/public_home_page.tsx
+++ web/static/js/flow/public/public_home_page.tsx
@@ -78,8 +78,8 @@ export class PublicHomePage extends React.Component<any, any> {
 
     }else if(window.innerWidth > 1280){
 
-        this.setState({fontSizeLarge: 19});
-        this.setState({fontSizeSmall: 18});        
+        this.setState({fontSizeLarge: 16});
+        this.setState({fontSizeSmall: 15});        
 
     }else if(window.innerWidth < 1280){
 
