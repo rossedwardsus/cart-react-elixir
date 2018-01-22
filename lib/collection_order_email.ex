125a2af6c48933a7c526d72e4bbe5e9ad713f397
Ross Edwards <rossedwards@Rosss-MacBook-Pro.local>
2018-01-22 14:17:05 -0800

    menu items and zipcodes

---
+++ lib/collection_order_email.ex
@@ -0,0 +1,58 @@
+defmodule Sconely.CollectionOrderEmail do
+  use Bamboo.Phoenix, view: SconeHomeElixir.UserOrderProcessedEmailView
+
+
+  def collection_order_email(_params) do
+
+
+  	#IO.puts("hello")
+  	#IO.inspect(_params)
+  	#IO.inspect(_params["order_contact_email"])
+    #IO.inspect(Map.fetch(_params, :order_contact_email))
+    #IO.inspect(System.get_env("MIX"))
+    
+    template = Phoenix.View.render_to_string(Sconely.YoursSocialPoolCompleteOrderEmailView, "yours_social_pool_complete_order_email.html", key: _params)
+
+    #IO.inspect(_params.args[:user_contact_email])
+
+    #send to admin as well
+
+    new_email(
+      #to: _params["order_contact_email"],
+      to: [_params.args[:user_contact_email]],
+      from: "orders@sconely.com",
+      subject: "Sconely.com " <> _params.collection_name <> " order: " <> _params.order_datetime_formatted,
+      html_body: template,
+      #html_body: "Thanks for your Sconely Order<br>pickup-smorgasburgh-payment method-contact-name-datetime-items" <> _params["order_id"],
+      #text_body: "Thanks for joining!"
+    )
+
+  end
+
+  
+
+  def collection_order_admin_email(_params) do
+
+  	#IO.puts("hello")
+  	#IO.inspect(_params)
+  	#IO.inspect(_params["order_contact_email"])
+    #IO.inspect(Map.fetch(_params, :order_contact_email))
+    #IO.inspect(System.get_env("MIX"))
+    
+    template = Phoenix.View.render_to_string(Sconely.YoursSocialPoolCompleteOrderAdminEmailView, "yours_social_pool_complete_order_admin_email.html", key: _params)
+
+    #IO.inspect(_params["order_first_name"])
+
+    #send to admin as well
+
+    new_email(
+      to: ["rossedwards.us@gmail.com", "julia@sconely.com"],
+      from: "orders@sconely.com",
+      subject: _params[:admin_email_subject],
+      html_body: template,
+      #html_body: "Thanks for your Sconely Order<br>pickup-smorgasburgh-payment method-contact-name-datetime-items" <> _params["order_id"],
+      #text_body: "Thanks for joining!"
+    )
+
+  end
+end
